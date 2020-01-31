b702b7a9e7ede99ed15dfd1761d18e28f52df0ee
Lamers <llamers@mcw.edu>
2020-01-31 11:47:44 -0600

    added run.sh

---
+++ src/main/dist/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Eva loading pipeline
+#
+. /etc/profile
+APPNAME=EvaPipeline
+SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
+
+APPDIR=/home/rgddata/pipelines/$APPNAME
+cd $APPDIR
+
+java -Dspring.config=$APPDIR/../properties/default_db2.xml \
+    -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
+    -jar lib/$APPNAME.jar "$@" > run.log 2>&1
+
+mailx -s "[$SERVER] Eva Pipeline Run" mtutaj@mcw.edu < $APPDIR/logs/summary.log
