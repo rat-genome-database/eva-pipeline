bf2d1dfd81ece3fdea4367e4fe813eb98d59fd4e
Lamers <llamers@mcw.edu>
2020-03-13 13:43:44 -0500

    creating padding base. testing with rat

--- src/main/java/edu/mcw/rgd/eva/Main.java
+++ src/main/java/edu/mcw/rgd/eva/Main.java
@@ -126,7 +126,7 @@ public class Main {
         Collection<Eva> tobeInserted = CollectionUtils.subtract(incoming, inRGD);
         if (!tobeInserted.isEmpty()) {
             logger.info("   New Eva objects to be inserted in chromosome "+chromosome+": " + tobeInserted.size());
-//            dao.insertEva(tobeInserted);
+            dao.insertEva(tobeInserted);
             tobeInserted.clear();
         }
 
@@ -134,7 +134,7 @@ public class Main {
         Collection<Eva> tobeDeleted = CollectionUtils.subtract(inRGD, incoming);
         if (!tobeDeleted.isEmpty()) {
             logger.info("   Old Eva objects to be deleted in chromosome "+chromosome+": " + tobeDeleted.size());
-//            dao.deleteEvaBatch(tobeDeleted);
+            dao.deleteEvaBatch(tobeDeleted);
             tobeDeleted.clear();
         }
 
