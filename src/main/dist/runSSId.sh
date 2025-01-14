fa9ed8027735071f967e8c718a0b65058e64cedb
llamersmcw <llamers@mcw.edu>
2025-01-14 08:43:52 -0600

    Update runSSId.sh

--- src/main/dist/runSSId.sh
+++ src/main/dist/runSSId.sh
@@ -6,6 +6,8 @@
 APPNAME=EvaPipeline
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 
+EMAILLIST=mtutaj@mcw.edu,llamers@mcw.edu
+
 APPDIR=/home/rgddata/pipelines/$APPNAME
 cd $APPDIR
 
@@ -13,4 +15,4 @@ java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
     -Xmx20g -jar lib/$APPNAME.jar --importEvaSSIds"$@" > ssIdRun.log 2>&1
 
-mailx -s "[$SERVER] Eva Pipeline Run" mtutaj@mcw.edu,llamers@mcw.edu < $APPDIR/logs/ssIdSummary.log
+mailx -s "[$SERVER] Eva Pipeline Run" $EMAILLIST < $APPDIR/logs/ssIdSummary.log
